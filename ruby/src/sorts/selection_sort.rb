252f4f015126cf3489199a7b2cd636793a1cf194
zckeyser <zckeyser@gmail.com>
2016-09-05 11:24:29 -0400

    [ruby] added selection sort

--- ruby/src/sorts/selection_sort.rb
+++ ruby/src/sorts/selection_sort.rb
@@ -1,5 +1,18 @@
 module SelectionSort
     def SelectionSort.sort(arr)
-        arr
+        # copy the input array
+        out = arr.map { |x| x }
+
+        for i in (0...out.length)
+            min = i
+            for j in (i...out.length)
+                if out[j] < out[min]
+                    min = j
+                end
+            end
+            out[i], out[min] = out[min], out[i]
+        end
+
+        out
     end
 end
