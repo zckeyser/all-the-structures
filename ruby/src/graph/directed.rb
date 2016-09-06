93ef795567bab53f4a274e62eceefdf4d40899a5
Chris Keyser <ckeyser@emoneyadvisor.com>
2016-09-06 11:47:39 -0400

    [ruby] added directed graph node

---
+++ ruby/src/graph/directed.rb
@@ -0,0 +1,22 @@
+class DirectedGraphNode
+    :attr value
+
+    def initialize(value)
+        @value = value
+        @neighbors = []
+    end
+
+    def neighbors
+        @neighbors
+    end
+
+    def add_neighbor(node)
+        raise ArgumentError, 'can only add a DirectedGraphNode as a neighbor to a DirectedGraphNode' if !node.is_a?(DirectedGraphNode)
+
+        @neighbors << node
+    end
+
+    def remove_neighbor(node)
+        @neighbors -= [node]
+    end
+end
