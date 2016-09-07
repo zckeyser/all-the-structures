e67827310fab7a05c26e4250b14415a575dbbf4f
zckeyser <zckeyser@gmail.com>
2016-09-07 01:00:56 -0400

    [ruby] finished bst implementation, still need traversals though

--- ruby/src/trees/bst.rb
+++ ruby/src/trees/bst.rb
@@ -1,15 +1,19 @@
 class BinarySearchTree
-    attr_reader :left, :right, :parent, :value
+    attr_reader :left, :right, :parent, :count
+    attr :value
 
     def initialize(value, parent)
         @left = nil
         @right = nil
         @parent = parent
         @value = value
+        @count = 1
     end
 
     def insert(val)
-        if val > @value
+        if val == @value
+            @count += 1
+        elsif val > @value
             if @right.nil?
                 @right = BinarySearchTree.new(val, self)
             else
@@ -25,7 +29,41 @@ class BinarySearchTree
     end
 
     def remove(val)
-        
+        if val == @value
+            # if this number's been inserted multiple times,
+            # remove one instance
+            if @count > 1
+                @count -= 1
+            elsif self.is_leaf?
+                # just null the reference to this in our parent
+                @parent.replace_child(@value, nil)
+            elsif self.has_single_child?
+                child = (@left || @right)
+
+                # replace this node with its only parent
+                @parent.replace_child(@value, child)
+            else
+                # replace this with the greatest value in its left subtree
+                child = @left.max
+
+                # move the child to this node
+                @value = child.value
+                @count = child.count
+
+                # get rid of the leftover object that we moved up, maintaining BST properties
+                child.remove(child.value)
+            end
+        elsif val < @value
+            # remove from left subtree
+            if !@left.nil?
+                @left.remove(val)
+            end
+        else
+            # remove from right subtree
+            if !@right.nil?
+                @right.remove(val)
+            end
+        end
     end
 
     def contains?(val)
@@ -39,10 +77,23 @@ class BinarySearchTree
     end
 
     def is_leaf?()
-
+        return @left.nil? && @right.nil?
     end
 
     def has_single_child?()
-
+        return @left.nil? ^ @right.nil?
     end
+
+    protected
+        def replace_child(child_val, replacement_node)
+            if child_val < value
+                @left = replacement_node
+            else
+                @right = replacement_node
+            end
+        end
+
+        def max
+            return @right.max if !@right.nil? else return self
+        end
 end
---
+++ ruby/test/trees/bst_test.rb
@@ -0,0 +1,72 @@
+require_relative '../../src/trees/bst'
+require_relative '../../src/trees/traversal'
+
+require 'test_helper'
+require 'minitest/autorun'
+require 'minitest/spec'
+
+describe BinarySearchTree do
+    before do
+        @root = BinarySearchTree.new(50, nil)
+
+        @root.insert(10)
+        @root.insert(30)
+        @root.insert(42)
+        @root.insert(15)
+        @root.insert(3)
+        @root.insert(60)
+        @root.insert(80)
+        @root.insert(83)
+        @root.insert(24)
+        @root.insert(73)
+        @root.insert(87)
+        @root.insert(100)
+    end
+
+    describe 'insert' do
+        it 'inserts values correctly' do
+            expected = '3 10 15 24 30 42 50 60 73 80 83 87 100'
+            TreeTraversal.inorder(@root).must_equal expected
+        end
+    end
+
+    describe 'remove' do
+        it 'removes a leaf correctly' do
+            @root.remove(3)
+
+            expected = '10 15 24 30 42 50 60 73 80 83 87 100'
+            TreeTraversal.inorder(@root).must_equal expected
+        end
+
+        it 'removes a node with a single child correctly' do
+            @root.remove(10)
+
+            expected = '3 15 24 30 42 50 60 73 80 83 87 100'
+            TreeTraversal.inorder(@root).must_equal expected
+        end
+
+        it 'removes a node with two children correctly' do
+            @root.remove(30)
+
+            expected = '3 10 15 24 42 50 60 73 80 83 87 100'
+            TreeTraversal.inorder(@root).must_equal expected
+        end
+
+        it 'removes the root correctly' do
+            @root.remove(50)
+
+            expected = '3 10 15 24 30 42 60 73 80 83 87 100'
+            TreeTraversal.inorder(@root).must_equal expected
+        end
+    end
+
+    describe 'contains' do
+        it 'finds a contained element' do
+            @root.contains?(80).must_equal true
+        end
+
+        it 'does not find an uncontained element' do
+            @root.contains?(150).must_equal false
+        end
+    end
+end
