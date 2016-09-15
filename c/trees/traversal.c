6f2480f72d9d623e5382695b4ec8f0fdae62d3dd
zckeyser <zckeyser@gmail.com>
2016-09-15 15:25:44 -0400

    \nfixed traversals to not need sanitized output

--- c/trees/traversal.c
+++ c/trees/traversal.c
@@ -13,36 +13,33 @@ void inorder_bst(char *out, BinarySearchTree *root);
 void postorder_bst(char *out, BinarySearchTree *root);
 
 // for sanitizing and copying over output
-void sanitize(char *out, char *in);
+void copy(char *out, char *in);
 
 void Traversal_preorder(char *out, BinaryTree *root) {
     char *result = malloc((sizeof(char) * BinaryTree_size(root) * 15) + 1);
+    result[0] = '\0';
 
     preorder(result, root);
 
-    sanitize(out, result);
+    copy(out, result);
 }
 
 void Traversal_inorder(char *out, BinaryTree *root) {
     char *result = malloc((sizeof(char) * BinaryTree_size(root) * 15) + 1);
+    result[0] = '\0';
 
     inorder(result, root);
 
-    sanitize(out, result);
+    copy(out, result);
 }
 
 void Traversal_postorder(char *out, BinaryTree *root) {
     char *result = malloc((sizeof(char) * BinaryTree_size(root) * 15) + 1);
+    result[0] = '\0';
 
     postorder(result, root);
 
-    // copy the string over
-    realloc(out, (sizeof(char) * strlen(result)));
-
-    strncpy(out, result, strlen(result));
-
-    // set the termination character at the last character, since it'll be an extra space
-    out[strlen(out) - 1] = 0;
+    copy(out, result);
 }
 
 void preorder(char *out, BinaryTree *root) {
@@ -93,32 +90,29 @@ void postorder(char *out, BinaryTree *root) {
 
 void Traversal_preorder_bst(char *out, BinarySearchTree *root) {
     char *result = malloc((sizeof(char) * BinarySearchTree_size(root) * 15) + 1);
+    result[0] = '\0';
 
     preorder_bst(result, root);
 
-    sanitize(out, result);
+    copy(out, result);
 }
 
 void Traversal_inorder_bst(char *out, BinarySearchTree *root) {
     char *result = malloc((sizeof(char) * BinarySearchTree_size(root) * 15) + 1);
+    result[0] = '\0';
 
     inorder_bst(result, root);
 
-    sanitize(out, result);
+    copy(out, result);
 }
 
 void Traversal_postorder_bst(char *out, BinarySearchTree *root) {
     char *result = malloc((sizeof(char) * BinarySearchTree_size(root) * 15) + 1);
+    result[0] = '\0';
 
     postorder_bst(result, root);
 
-    // copy the string over
-    realloc(out, (sizeof(char) * strlen(result)));
-
-    strncpy(out, result, strlen(result));
-
-    // set the termination character at the last character, since it'll be an extra space
-    out[strlen(out) - 1] = 0;
+    copy(out, result);
 }
 
 void preorder_bst(char *out, BinarySearchTree *root) {
@@ -167,17 +161,11 @@ void postorder_bst(char *out, BinarySearchTree *root) {
     strncat(out, val, 15);
 }
 
+void copy(char *out, char *in) {
+    // copy the string over
+    realloc(out, (sizeof(char) * strlen(in)));
 
-void sanitize(char *out, char *in) {
-    // for some reason 3 random characters were appearing at the beginning of the
-    // output string, so we're trimming the first 3 characters off when moving to output
-    for(int i = 3; i < strlen(in); i++) {
-        in[i - 3] = in[i];
-    }
-
-    realloc(out, (sizeof(char) * strlen(in) - 2));
-
-    strncpy(out, in, strlen(in) - 3);
+    strncpy(out, in, strlen(in));
 
     // set the termination character at the last character, since it'll be an extra space
     out[strlen(out) - 1] = 0;
