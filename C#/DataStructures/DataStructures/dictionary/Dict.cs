4fe27970eccfb77fcf94c8d1a8d341f543a419ce
zckeyser <zckeyser@gmail.com>
2016-09-26 02:30:29 -0400

    [C#] added dictionary

---
+++ C#/DataStructures/DataStructures/dictionary/Dict.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace DataStructures.dictionary
+{
+    public class Dict<TKey, TValue> : IEnumerable<System.Collections.Generic.KeyValuePair<TKey, TValue>>
+    {
+        private const int DEFAULT_BUCKET_COUNT = 1024;
+
+        #region KeyValuePair
+        /// <summary>
+        /// class used to represent the key-value pairs in buckets,
+        /// which use the linked list strategy for collisions (as opposed to rollover)
+        /// </summary>
+        private class KeyValuePair<UKey, UValue>
+        {
+            public UKey Key { get; set; }
+            public UValue Value { get; set; }
+            public KeyValuePair<UKey, UValue> Next { get; set; }
+
+            public KeyValuePair(UKey key, UValue value)
+            {
+                Key = key;
+                Value = value;
+            }
+        }
+        #endregion
+
+        private KeyValuePair<TKey, TValue>[] Data { get; set; }
+
+        public int Size
+        {
+            get
+            {
+                int size = 0;
+
+                for(int i = 0; i < Data.Length; i++)
+                {
+                    var curr = Data[i];
+
+                    // sum size of bucket
+                    while (curr != null) size++; curr = curr.Next;
+                }
+
+                return size;
+            }
+        }
+
+        public Dict()
+        {
+            Data = new KeyValuePair<TKey, TValue>[DEFAULT_BUCKET_COUNT];
+        }
+
+        public TValue this[TKey key]
+        {
+            get
+            {
+                return Get(key);
+            }
+            set
+            {
+                Set(key, value);
+            }
+        }
+
+        private void Set(TKey key, TValue value)
+        {
+            int index = key.GetHashCode() % Data.Length;
+
+            if (Data[index] == null || Data[index].Key.Equals(key))
+                Data[index] = new KeyValuePair<TKey, TValue>(key, value);
+            else
+            {
+                // collision -- try to find the value in the list to 
+                // override or else add it to the end of the bucket
+                var curr = Data[index];
+
+                while(curr.Next != null)
+                {
+                    // overwrite the existing value if this key is already in the dict
+                    if(curr.Next.Key.Equals(key))
+                    {
+                        curr.Next.Value = value;
+                        return;
+                    }
+
+                    curr = curr.Next;
+                }
+
+                // we didn't find this key, so we'll add it
+                curr.Next = new KeyValuePair<TKey, TValue>(key, value);
+            }
+        }
+
+        private TValue Get(TKey key)
+        {
+            var index = key.GetHashCode() % Data.Length;
+
+            if (Data[index] == null)
+                throw new ArgumentException(string.Format("Key %s was not found in Dict on Get", key.ToString()));
+            else if (Data[index].Key.Equals(key))
+                return Data[index].Value;
+            else
+            {
+                // it's a collision -- we'll need to try 
+                // to find it in the bucket
+
+                var curr = Data[index].Next;
+
+                while(curr != null)
+                {
+                    if (curr.Key.Equals(key))
+                        return curr.Value;
+
+                    curr = curr.Next;
+                }
+
+                // we couldn't find it
+                throw new ArgumentException(string.Format("Key %s was not found in Dict on Get", key.ToString()));
+            }
+        }
+
+        public bool ContainsKey(TKey key)
+        {
+            var index = key.GetHashCode() % Data.Length;
+
+            if (Data[index] == null)
+                return false;
+            else if (Data[index].Key.Equals(key))
+                return true;
+            else
+            {
+                // it's a collision -- we'll need to try 
+                // to find it in the bucket
+
+                var curr = Data[index].Next;
+
+                while (curr != null)
+                {
+                    if (curr.Key.Equals(key))
+                        return true;
+
+                    curr = curr.Next;
+                }
+
+                // we couldn't find it
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// clear all key-value pairs from the Dict
+        /// </summary>
+        public void Clear()
+        {
+            for (int i = 0; i < Data.Length; i++)
+                Data[i] = null;
+        }
+
+        IEnumerator<System.Collections.Generic.KeyValuePair<TKey, TValue>> IEnumerable<System.Collections.Generic.KeyValuePair<TKey, TValue>>.GetEnumerator()
+        {
+            foreach(var bucket in Data)
+            {
+                var curr = bucket;
+
+                while(curr != null)
+                {
+                    yield return new System.Collections.Generic.KeyValuePair<TKey, TValue>(curr.Key, curr.Value);
+                    curr = curr.Next;
+                }
+            }
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            foreach (var bucket in Data)
+            {
+                var curr = bucket;
+
+                while (curr != null)
+                {
+                    yield return new System.Collections.Generic.KeyValuePair<TKey, TValue>(curr.Key, curr.Value);
+                    curr = curr.Next;
+                }
+            }
+        }
+    }
+}
---
+++ C#/DataStructures/Test/dictionary/DictTest.cs
@@ -0,0 +1,116 @@
+﻿using DataStructures.dictionary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Test.dictionary
+{
+    [TestClass]
+    public class DictTest
+    {
+        private Dict<string, int> dict;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            dict = new Dict<string, int>();
+        }
+
+        [TestMethod]
+        public void Dict_InitializesEmpty()
+        {
+            Assert.AreEqual(0, dict.Size);
+        }
+
+        [TestMethod]
+        public void Dict_SingleGetSet()
+        {
+            dict["foo"] = 10;
+
+            Assert.AreEqual(10, dict["foo"]);
+        }
+
+        [TestMethod]
+        public void Dict_SizeAfterSingleSet()
+        {
+            dict["foo"] = 10;
+
+            Assert.AreEqual(1, dict.Size);
+        }
+
+        [TestMethod]
+        public void Dict_MultipleGetSet()
+        {
+            dict["foo"] = 10;
+            dict["bar"] = 30;
+            dict["buzz"] = 50;
+
+            Assert.AreEqual(10, dict["foo"]);
+            Assert.AreEqual(30, dict["bar"]);
+            Assert.AreEqual(50, dict["buzz"]);
+        }
+
+        [TestMethod]
+        public void Dict_SizeAfterMultipleSet()
+        {
+            dict["foo"] = 10;
+            dict["bar"] = 30;
+            dict["buzz"] = 50;
+
+            Assert.AreEqual(3, dict.Size);
+        }
+
+        [TestMethod]
+        public void Dict_ManyGetSet()
+        {
+            var rand = new Random();
+
+            var pairs = new List<KeyValuePair<string, int>>();
+
+            // build out key value pairs and add them
+            foreach(var i in Enumerable.Repeat(0, 5000))
+            {
+                var key = new Guid().ToString();
+                var value = rand.Next();
+                pairs.Add(new KeyValuePair<string, int>(key, value));
+
+                dict[key] = value;
+            }
+
+            // check that the key value pairs are valid when accessed in dict
+            foreach(var pair in pairs)
+            {
+                Assert.AreEqual(pair.Value, dict[pair.Key]);
+            }
+        }
+
+        [TestMethod]
+        public void Dict_ContainsKey_Positive()
+        {
+            dict["foo"] = 10;
+
+            Assert.IsTrue(dict.ContainsKey("foo"));
+        }
+
+        [TestMethod]
+        public void Dict_ContainsKey_Negative()
+        {
+            dict["foo"] = 10;
+
+            Assert.IsFalse(dict.ContainsKey("bar"));
+        }
+
+        [TestMethod]
+        public void Dict_Clear()
+        {
+            dict["foo"] = 10;
+            dict["bar"] = 30;
+            dict["buzz"] = 50;
+
+            dict.Clear();
+
+            Assert.AreEqual(0, dict.Size);
+        }
+    }
+}
