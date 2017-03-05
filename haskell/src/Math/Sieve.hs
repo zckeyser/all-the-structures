4d4d84bdc9c9afb09882af425e8da426ac6edbed
zckeyser <zckeyser@gmail.com>
2017-03-05 01:07:34 -0500

    fixed haskell sieve implementation

--- haskell/src/Math/Sieve.hs
+++ haskell/src/Math/Sieve.hs
@@ -19,7 +19,11 @@ divisible divisor num = num `mod` divisor == 0
 eratosthenesHelper :: [Integer] -> Divisor -> [Prime]
 eratosthenesHelper primes divisor
     | divisor > maximum primes `div` 2 = primes
-    | otherwise                        = let filteredPrimes = filter (divisible divisor) primes
+    | otherwise                        = let filteredPrimes = filter (\x -> or
+                                                                          [ not . divisible divisor $ x -- make sure it isn't divisible
+                                                                          , x <= divisor -- only try to filter numbers which are greater than the divisor
+                                                                          ])
+                                                                     primes
                                          in eratosthenesHelper filteredPrimes $ nextDivisor divisor filteredPrimes
 
 nextDivisor :: Divisor -> ([Integer] -> Divisor)
