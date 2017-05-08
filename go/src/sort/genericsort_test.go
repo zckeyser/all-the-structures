14a1a527f3957b586b6383d18b2e5095565c211c
zckeyser <zckeyser@gmail.com>
2017-05-08 05:14:38 -0400

    added generic sort testing interface

--- go/src/sort/genericsort_test.go
+++ go/src/sort/genericsort_test.go
@@ -1,9 +1,38 @@
 package mysort
 
 import "testing"
+import "strings"
 
-type myInt struct {
-  value int
+func (n int) Less(other interface{}) bool {
+  switch t := other.(type) {
+  default:
+    return false
+  case int:
+    return n < t
+  }
+}
+
+func (s string) Less(other interface{}) bool {
+  switch t := other.(type) {
+  default:
+    return false
+  case string:
+    return strings.Compare(s, t) < 0
+  }
+}
+
+type person struct {
+  name string
+  age int
+}
+
+func Less(p person) (other interface{}) bool {
+  switch t := other.(type) {
+  default:
+    return false
+  case person:
+    return p.age < t.age || strings.Compare(p.name, t.name) < 0
+  }
 }
 
 func IntSortTesting(t *testing.T) {
